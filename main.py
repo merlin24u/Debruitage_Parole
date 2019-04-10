606b0c1c2ac4de504445433a307c451008aadefc
Paul Merlin <paul.merlin1@etu.univ-lorraine.fr>
2019-04-10 11:38:02 +0200

    mise en place debruitage mais ne fonctionne pas

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from scipy.io.wavfile import read
 from scipy.io.wavfile import write
-from math import log10
-from math import cos
-from math import pi
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.fft as FFT
@@ -27,8 +25,8 @@ def affiche(rate, data):
 def fenetrage_hamming(size):
     hamming = []
     for n in range(0, size):
-        angle = 2 * pi * n/size
-        hamming.append(0.54 - 0.46 * cos(angle))
+        angle = 2 * math.pi * n/size
+        hamming.append(0.54 - 0.46 * math.cos(angle))
 
     return hamming
 
@@ -43,7 +41,7 @@ def spectre_amplitude(spectre):
     res = np.abs(spectre)
     spec_affichage = res
     res = [20*x for x in res]
-    spec_affichage = [20*log10(x) for x in spec_affichage]
+    spec_affichage = [20*math.log10(x) for x in spec_affichage]
 
     return res, spec_affichage
 
@@ -52,7 +50,21 @@ def spectre_phase(spectre):
 
 def spectre_reconstruction(spectre_amplitude, spectre_phase):
     return spectre_amplitude * np.exp(1j * spectre_phase)
-    
+
+def soustraction_spectrale(spectre_amplitude, bruit):
+    alpha = 2
+    beta = 1
+    gamma = 0
+    res = np.zeros(len(spectre_amplitude))
+    for i in range(0, len(spectre_amplitude)):
+        tmp = math.pow(math.pow(spectre_amplitude[i], alpha) - beta * math.pow(bruit, alpha), 1/alpha)
+        if tmp > 0:
+            res[i] = tmp
+        else:
+            res[i] = gamma * bruit[i]
+
+    return res
+        
 def modif_signal(rate, signal, m, N):
     signal_modif = np.zeros(len(signal))
     somme_hamming = np.zeros(len(signal))
@@ -66,6 +78,13 @@ def modif_signal(rate, signal, m, N):
         amplitude, ampli_aff = spectre_amplitude(spectre)
         tab_spectres.append(ampli_aff)
         phase = spectre_phase(spectre)
+        # modification spectre d'amplitude
+        bruit = 0
+        for j in range(0, 5):
+            bruit += amplitude[j]
+        bruit /= 5
+        amplitude = soustraction_spectrale(amplitude, bruit)
+        # reconstruction du signal
         spectre = spectre_reconstruction(amplitude, phase)
         fenetre = np.real(FFT.ifft(spectre, fftsize))
         signal_modif[i:i+N] += fenetre[0:N]
@@ -79,4 +98,4 @@ def modif_signal(rate, signal, m, N):
         
     
 if __name__ == "__main__":
-    main("test_seg.wav")
+    main("test_seg_bruit_0dB.wav")
